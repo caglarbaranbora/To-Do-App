e406e9f348e6fab0a2fad06d568d5cf61e7ef026
caglarbaranbora <boracaglarbaran@gmail.com>
2025-03-05 16:18:30 +0300

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 export default function page() {
+  const [task, setTask] = useState([]);
+
+  const handleClick = () => {
+    const inputBox = document.getElementById("input-box");
+    const inputValue = inputBox.value;
+    if (inputValue === "") {
+      alert("Please enter a task");
+    } else {
+      setTask([...task, inputValue]);
+      inputBox.value = "";
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newTask = task.filter((_, i) => i !== index);
+    setTask(newTask);
+  };
   return (
     <div className="w-full min-h-[100vh] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-10">
       <div className="w-full max-w-[540px] bg-white mt-[100px] mb-[20px] mx-auto px-[30px] pt-[40px] pb-[70px] rounded-[10px]">
@@ -9,22 +27,35 @@ export default function page() {
         </h2>
         <div className="flex align-center justify-between bg-[#edeef0] rounded-[30px] pl-[20px] mb-[25px]">
           <input
-            className="flex outline-none border-none p-[10px] bg-transparent"
+            className="flex outline-none border-none p-[10px] bg-transparent text-[#002765] font-bold"
             type="text"
             placeholder="Add a new task"
             id="input-box"
           />
-          <button className="border-none outline-none px-[50px] py-[16px] bg-[#ff5945] color-white text-[16px] cursor-pointer rounded-[40px]">
+          <button
+            onClick={handleClick}
+            className="border-none outline-none px-[50px] py-[16px] bg-[#ff5945] color-white text-[16px] cursor-pointer rounded-[40px]"
+          >
             ADD
           </button>
         </div>
-        <ul
-          id="list-container"
-          className="list-none text-[17px] pt-[12px] pr-[50px] pb-[12px] pl-[8px] select-none relative before:content-none before:absolute before:height-[28px] before:rounded-[50%] before:top-[12px] before:content bg-[url('/images/unchecked.png')] before:bg-size-[contain]"
-        >
-          <li>task 1</li>
-          <li>task 2</li>
-          <li>task 3</li>
+        <ul>
+          <li>
+            {task.map((task, index) => (
+              <div
+                key={index}
+                className="flex justify-between align-center bg-[#edeef0] rounded-[30px] pl-[20px] mb-[10px] py-[10px] text-[#002765] font-bold"
+              >
+                {task}
+                <button
+                  className="border-none outline-none px-[20px] py-[10px] bg-[#ff5945] color-white text-[16px] cursor-pointer rounded-[40px]"
+                  onClick={() => handleDelete(index)}
+                >
+                  DELETE
+                </button>
+              </div>
+            ))}
+          </li>
         </ul>
       </div>
     </div>
